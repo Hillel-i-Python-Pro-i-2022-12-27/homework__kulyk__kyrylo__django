35caf894ec4557918324a61e24449483dd32a80e
ptizaS <ptizay@gmail.com>
2023-02-28 14:09:28 +0100

    Completed homework 13 (add Env, Postgres, Docker Compose profiles)

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 ![example workflow](https://github.com/Hillel-i-Python-Pro-i-2022-12-27/homework__kulyk__kyrylo__hw5/actions/workflows/main-workflow.yml/badge.svg)
 ### Student [Hillel IT School](https://ithillel.ua/), I am from Odessa.
 ***
-## 📝Homework Django
+## 📝Homework Django framework
 Completing homework tasks at Django framework.
 ***
 ### ▶️Run
@@ -26,7 +26,7 @@ make init-dev
 Use services in dockers.
 ### ▶️Run
 Make all actions needed for run homework from zero,  
-making migrate and generate 100 contacts
+making migrate
 ```shell
 make d-homework-i-run
 ```
@@ -54,6 +54,11 @@ make init-dev-i-delete-superuser
 ```
 ***
 ## 🗄 DataBase
+### ▶️Run db
+Run database for local dev
+```shell
+make d-run-i-local-dev
+```
 ### 🧳Make migration
 Created migration file
 ```shell
--- core/settings.py
+++ core/settings.py
@@ -12,26 +12,34 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+# noinspection PyUnresolvedReferences
+import environ
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 APPS_DIR = BASE_DIR.joinpath("apps")
 
+env = environ.Env()
+env.read_env(BASE_DIR.joinpath(".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-oi5#b@=pvh1ocu77joi=@zo4yxmsn_-_7$+jf3j6w^x#$n7)ly"
+SECRET_KEY = env.str("DJANGO__SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DJANGO__DEBUG", False)
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "0.0.0.0",
-    "127.0.0.1",
-]
-
+ALLOWED_HOSTS = env.list("DJANGO__ALLOWED_HOSTS", default=[])
+if DEBUG:
+    ALLOWED_HOSTS.extend(
+        [
+            "localhost",
+            "0.0.0.0",
+            "127.0.0.1",
+        ]
+    )
 
 # Application definition
 
@@ -51,7 +59,10 @@ LOCAL_APPS = [
     "apps.users",
 ]
 
-THIRD_PARTY_APPS = []
+THIRD_PARTY_APPS = [
+    "crispy_forms",
+    "crispy_bootstrap5",
+]
 
 INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_PARTY_APPS
 
@@ -85,6 +96,10 @@ TEMPLATES = [
     },
 ]
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 WSGI_APPLICATION = "core.wsgi.application"
 
 
@@ -92,10 +107,15 @@ WSGI_APPLICATION = "core.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db" / "db.sqlite3",
-    }
+    "default": env.db_url_config(
+        # f'sqlite:///{BASE_DIR.joinpath("db", "db.sqlite3")}'
+        # postgres://user:password@host:port/dbname
+        env.str(
+            "DJANGO__DB_URL",
+            f'postgres://{env.str("POSTGRES_USER")}:{env.str("POSTGRES_PASSWORD")}'
+            f'@{env.str("POSTGRES_HOST")}:{env.str("POSTGRES_PORT")}/{env.str("POSTGRES_DB")}',
+        )
+    )
 }
 
 
@@ -134,6 +154,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = BASE_DIR / "static"
+
+STATICFILES_DIRS = [
+    APPS_DIR / "static",
+]
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

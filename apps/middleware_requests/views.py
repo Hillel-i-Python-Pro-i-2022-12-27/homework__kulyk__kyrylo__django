e5f5b64ec13428d46db1df6db2377e912979e62b
ptizaS <ptizay@gmail.com>
2023-03-12 15:39:39 +0100

    Completing homework 16 (add count all logs)

--- apps/middleware_requests/views.py
+++ apps/middleware_requests/views.py
@@ -5,11 +5,12 @@ from apps.middleware_requests.models import HttpRequestsLog
 
 class LogListView(ListView):
     model = HttpRequestsLog
-    template_name = "middleware_requests/logs/logs_list.html"
+    template_name = "middleware_requests/logs_list.html"
     queryset = HttpRequestsLog.objects.all().order_by("-created_time")
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
+        count_all_logs = HttpRequestsLog.objects.count()
         context["title"] = "Logs List"
+        context["count_all_logs"] = count_all_logs
         return context
